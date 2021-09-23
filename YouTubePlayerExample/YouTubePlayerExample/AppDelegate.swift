29a7e7da6976f6390690741d928d248a3b0bb395
Wisnu Wardana <wisnu.wardana@allianz.co.id>
2021-09-23 12:59:44 +0700

    first init

---
+++ YouTubePlayerExample/YouTubePlayerExample/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  YouTubePlayerExample
+//
+//  Created by Giles Van Gruisen on 1/31/15.
+//  Copyright (c) 2015 Giles Van Gruisen. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
